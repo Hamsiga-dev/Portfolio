fd0b9030d0e6c2c771bcd00d9c7a5c5bf7b63b84
Hamsiga-dev <srihamsiga.dev@gmail.com>
2026-02-01 17:17:36 +0530

    header.js, resume.js issue fixed

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,27 +1,62 @@
+// import ResumeImg from '../assets/resume.jpg';
+
+
+// export default function Resume(){
+//     const config = {
+//         link: '/Hamsiga Srikanthaverl - ATS.pdf'
+//     }
+
+
+
+//     return <section id='resume' className='flex flex-col md:flex-row bg-secondary px-5' >
+//         <div className='py-5 md:w-1/2 flex justify-end'>
+//             <img className='w-[400px]' src={ResumeImg} />
+
+//         </div>
+
+//         <div className='md:w-1/2 flex justify-center'>
+//             <div className='flex flex-col justify-center'>
+//                 <h1 className='text-4xl text-white border-b-4 border-primary mb-5 w-[140px] font-bold'>Resume </h1>
+//                 <p className='text-white pb-5'>You can view my Resume. <a className='btn' href= {config.link}> Download </a></p>
+                
+//             </div>
+
+
+//         </div>
+//     </section>
+// }
+
 import ResumeImg from '../assets/resume.jpg';
 
-
-export default function Resume(){
+export default function Resume() {
     const config = {
         link: '/Hamsiga Srikanthaverl - ATS.pdf'
     }
 
-
-
-    return <section id='resume' className='flex flex-col md:flex-row bg-secondary px-5' >
-        <div className='py-5 md:w-1/2 flex justify-end'>
-            <img className='w-[400px]' src={ResumeImg} />
-
-        </div>
-
-        <div className='md:w-1/2 flex justify-center'>
-            <div className='flex flex-col justify-center'>
-                <h1 className='text-4xl text-white border-b-4 border-primary mb-5 w-[140px] font-bold'>Resume </h1>
-                <p className='text-white pb-5'>You can view my Resume. <a className='btn' href= {config.link}> Download </a></p>
-                
+    return (
+        <section id='resume' className='flex flex-col md:flex-row bg-secondary px-5'>
+            <div className='py-5 md:w-1/2 flex justify-end'>
+                {/* Added alt text for accessibility */}
+                <img
+                    className='w-[400px]'
+                    src={ResumeImg}
+                    alt="Hamsiga Srikanthaverl Resume"
+                />
             </div>
 
-
-        </div>
-    </section>
+            <div className='md:w-1/2 flex justify-center'>
+                <div className='flex flex-col justify-center'>
+                    <h1 className='text-4xl text-white border-b-4 border-primary mb-5 w-[140px] font-bold'>
+                        Resume
+                    </h1>
+                    <p className='text-white pb-5'>
+                        You can view my Resume.{' '}
+                        <a className='btn' href={config.link}>
+                            Download
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
 }
