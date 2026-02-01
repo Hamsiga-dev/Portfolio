6f369dbc607706756cb05cb20d7ab8604948f67e
Hamsiga-dev <srihamsiga.dev@gmail.com>
2026-02-01 17:11:05 +0530

    navigation button href issue fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,17 +10,17 @@ export default function Header() {
         <a className="font-bold text-black" href="#">Hamsiga Srikanthaverl</a>
         <nav className="hidden md:block">
             <ul className="flex  text-white">
-                <li><a href="/home">Home</a></li>
-                <li><a href="/#about">About</a></li>
-                <li><a href="/#projects">Projects</a></li>
-                <li><a href="/#resume">Resume</a></li>
-                <li><a href="/#contact">Contact</a></li>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#resume">Resume</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
 
         {toggleMenu && <nav className="block  md:hidden ">
             <ul onClick={() => setToggleMenu(!toggleMenu)} className="flex  flex-col text-white mobile-nav">
-                <li><a href="/home">Home</a></li>
+                <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#resume">Resume</a></li>
